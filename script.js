76c5bb64d526b58a05a9c91a2ae24618c1e2e617
Rakan1166 <rakanhossein4@gmail.com>
2024-11-18 19:28:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function startMonitoring() {
+    alert('Starting eye monitoring... (Feature in development)');
+}
+
+function showReports() {
+    alert('Displaying reports... (Feature in development)');
+}
+
+function startExercises() {
+    alert('Starting eye exercises... (Feature in development)');
+}
